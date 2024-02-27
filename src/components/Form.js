f085a4fe51b0412167e85f527e09be486e880232
Robinson Mourão <bob.info.guaratiba@gmail.com>
2024-02-27 10:10:18 -0300

    Usando hook useState()

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,39 @@
+import {useState} from 'react'
+
 function Form(){
 
     function cadastrarUsuario(evento){
         evento.preventDefault()
-        console.log("Cadastrou usuário")
+        console.log(`Usuário cadastrado com nome: '${name}' e senha: '${password}'`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+    
     return(
         <div>
             <h1>Meu cadastro:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome!"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        id="name"
+                        name="name"
+                        placeholder="Digite seu nome!"
+                        //value={name}
+                        onChange={(evento) => setName(evento.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        id="password"
+                        name="password"
+                        placeholder="Digite sua senha"
+                        onChange={(evento) => setPassword(evento.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="cadastrar"/>
